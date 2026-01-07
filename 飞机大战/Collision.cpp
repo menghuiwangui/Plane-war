69012cc861996ff4a57a5f8b15423504d55b9a05
menghuiwangui <2964668101@qq.com>
2026-01-07 12:29:40 +0800

    Delete 飞机大战 directory

--- 飞机大战/Collision.cpp
+++
@@ -1,6 +0,0 @@
-#include"Collision.h"
-bool IsCollide(int x1, int y1, int w1, int h1,
-	int x2, int y2, int w2, int h2)//检测两个矩形是否碰撞
-{
-	return !(x1 + w1<x2 || x1>x2 + w2 || y1 + h1<y2 || y1>y2 + h2);
-}
--- 飞机大战/MainInterface.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//сно╥╢╟©з
-#define WINDOWS_WIDTH 480   
-#define WINDOWS_HEIGTH 640
-
-int Load_Music();
-void Load_MainInterface();
